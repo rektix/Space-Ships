ede4b5cc9cc30ae0a12383bc167022f067216082
Mihajlo Krsmanović <mihajlo.krsmanovic@yahoo.com>
2016-12-12 11:21:37 +0100

    added style for mode select and opens game screen after selection

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-const $title = $("#title");
-const $game = $('#game');
-$title.on('click', function(){
+var title = $('#title');
+var select = $('#select');
+var game = $('#game');
+var p1 = $('#p1');
+var p2 = $('#p2');
+var mode;
+title.on('click', function(){
     $(this).hide();
-    $game.show();
+    select.show();
+});
+$('#playSingle').click(function(){
+    select.hide();
+    game.show();
+    p2.hide();
+    mode = 'single';
+});
+
+$('#playMulti').click(function(){
+    select.hide();
+    game.show();
+    mode = 'multi';
+    p1.css('left', '30%');
 });
