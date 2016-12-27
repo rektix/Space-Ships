ff835968b34e1823505d0e958a0498bce25a9844
Mihajlo Krsmanović <mihajlo.krsmanovic@yahoo.com>
2016-12-27 21:22:43 +0100

    added enemies

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ var select = $('#select');
 var game = $('#game');
 var p1 = $('#p1');
 var p2 = $('#p2');
-var mode, interval;
-var v1, v2;
+var mode, interval, interval1, score1 = 0, score2 = 0;
+var v1, v2, v = 2;
 var x1 = 320, x2 = 458, y1 = '320px', y2 = '458px', y3;
 var move1, move2, fire1, fire2;
+var enemyX = [50, 110, 170, 230, 290, 350, 410, 470, 530];
+var enemyY = [30, 70, 110]; 
 title.on('click', function(){
     $(this).hide();
     select.show();
@@ -16,15 +18,24 @@ $('#playSingle').click(function (){
     game.show();
     p2.hide();
     mode = 'single';
+    fire1 = false;
 });
 
 $('#playMulti').click(function(){
     select.hide();
     game.show();
+    $('#score2').show();
     mode = 'multi';
     p1.css('left', '192px');
     x1 = 192; y1 = '192px';
+    fire1 = false; fire2 = false;
 });
+
+for (var i = 0; i < 9; i++){
+    for (var j = 0; j < 3; j++){
+        $('#game').append("<div class='enemy', style='left: "+enemyX[i]+"px; top: "+enemyY[j]+"px;'></div>");
+    }
+}
 
 $(document).bind('keydown', function(e){
     if (e.which === 65) {
@@ -115,8 +126,46 @@ interval = setInterval(function () {
         p2.css('left', y2);
     }
 }, 15);
+interval1 = setInterval(function(){
+    if (mode === 'multi' || mode === 'single'){
+        $(".enemy").each(function(){
+            if (parseInt($(this).css('left'), 10) + v > 600){
+                v = -2;
+            }
+            if (parseInt($(this).css('left'), 10) + v < 0){
+                v = 2;
+            }
+        });
+        //console.log(v);
+        $(".enemy").each(function(){
+            $(this).css('left', parseInt($(this).css('left'), 10) + v + 'px');
+        })
+    }
+}, 30);
+interval2 = setInterval(function(){
+    $(".enemy").each(function(){
+        if (parseInt($(this).css('left'), 10) < parseInt($(".bullet1").css('left'), 10) + parseInt($(".bullet1").css('width'), 10) &&
+        parseInt($(this).css('left'), 10) + parseInt($(this).css('width'), 10) > parseInt($(".bullet1").css('left'), 10) &&
+        parseInt($(this).css('top'), 10) < parseInt($(".bullet1").css('top'), 10) + parseInt($(".bullet1").css('height'), 10) &&
+        parseInt($(this).css('height'), 10) + parseInt($(this).css('top'), 10) > parseInt($(".bullet1").css('top'), 10)){
+            $(this).remove();
+            $(".bullet1").remove();
+            score1 += 100;
+            $('#score1').text(score1);
+        }
+        if (parseInt($(this).css('left'), 10) < parseInt($(".bullet2").css('left'), 10) + parseInt($(".bullet2").css('width'), 10) &&
+        parseInt($(this).css('left'), 10) + parseInt($(this).css('width'), 10) > parseInt($(".bullet2").css('left'), 10) &&
+        parseInt($(this).css('top'), 10) < parseInt($(".bullet2").css('top'), 10) + parseInt($(".bullet2").css('height'), 10) &&
+        parseInt($(this).css('height'), 10) + parseInt($(this).css('top'), 10) > parseInt($(".bullet2").css('top'), 10)){
+            $(this).remove();
+            $(".bullet2").remove();
+            score2 += 100;
+            $('#score2').text(score2);
+        }
+    })
+}, 1);
 $(document).bind('keydown', function(e){    
-    if (e.which === 32) {
+    if (e.which === 32 && fire1 === false) {
         // space    
         fire1 = true;
         $('#game').append("<div class='bullet1 bullet', style='left: "+y1+"'></div>");
@@ -125,10 +174,11 @@ $(document).bind('keydown', function(e){
                     {
                         $(this).remove();
                     })
+        setTimeout(function (){fire1 = false;}, 1500);
     }
 });
 $(document).bind('keydown', function(e){ 
-    if (e.which === 16) {
+    if (e.which === 16 && fire2 === false) {
         // shift
         fire2 = true;        
         y3 = x2 + 24 + 'px';
@@ -138,5 +188,6 @@ $(document).bind('keydown', function(e){
                     {
                         $(this).remove();
                     })
+        setTimeout(function (){fire2 = false;}, 1500);
     }
 });
